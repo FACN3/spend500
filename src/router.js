a5c421f503a051a5b5e10879782aa83de3d9b201
MynahMarie <mynah.marie@gmail.com>
2017-12-12 18:06:26 +0200

    Create Homepage functionality
    Relates #11

--- src/router.js
+++ src/router.js
@@ -1,10 +1,13 @@
 const handleHome = require('./handlers/handleHome');
+const handleItems = require('./handlers/handleItems');
 const handleStatic = require('./handlers/handleStatic');
 
 const router = (req, res) => {
   const url = req.url;
   if (url === '/') {
     handleHome(req, res);
+  } else if (url === '/items') {
+    handleItems(req, res);
   } else {
     handleStatic(req, res);
   }
