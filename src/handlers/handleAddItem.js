2acadd4002095b993690618233a5a53af8c95cae
hoslack <hoslackochieng@gmail.com>
2017-12-14 13:05:38 +0200

    got userid from cookie so as to add to cart
    relates #22

--- src/handlers/handleAddItem.js
+++ src/handlers/handleAddItem.js
@@ -1,8 +1,12 @@
 const addtocart = require('../database/queries/addtocart');
 const qs = require('querystring');
+const jwtdecode = require('jwt-decode');
+
 
 const handleAddItem = (request,response)=>{
-	const userid = 3;
+	let v = request.headers.cookie;
+  	let w = jwtdecode(v);
+	const userid = w.userid;
 	let data ='';
 	request.on('data',(chunk)=>{
 		data+=chunk;
@@ -11,7 +15,7 @@ const handleAddItem = (request,response)=>{
 	request.on('end', ()=>{
 		const p_data = qs.parse(data);
 		const id = Object.keys(p_data)[0];
-		addtocart(3, id, (error,result)=>{
+		addtocart(userid, id, (error,result)=>{
 			if (error) {
 				console.log(error);
 			}else {
--- src/handlers/handleCart.js
+++ src/handlers/handleCart.js
@@ -1,8 +1,11 @@
+const jwtdecode = require('jwt-decode');
 const showcart = require('../database/queries/showcart');
 
 const handleCart = (request,response)=>{
-	const username = 'mynah';
-	showcart(username,(error,result)=>{
+	let v = request.headers.cookie;
+  	let w = jwtdecode(v);
+	const userid = w.userid;
+	showcart(userid,(error,result)=>{
 		if (error) {console.log(error);}
 		const stringItems = JSON.stringify(result.rows);
 		response.writeHead(200,{'Content-Type':'application/json'});
